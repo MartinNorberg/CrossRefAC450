9fafe191c77ce6c709374a0695930fff4a9ea7a4
MartinNorberg <martin.norberg@gautomation.se>
2020-01-12 11:44:58 +0100

    Initial commit

---
+++ ViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace AC450Communication
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel()
+        {
+
+        }
+        public ICommand BrowsePcPath { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
